165f7c304c994a63f9a9b7e0fc82a7bb75f11636
Jacob Hyde <jacobosity@gmail.com>
2020-04-07 03:37:06 +0000

    Done.

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,11 +1,7 @@
 # don't forget to add: require 'pry'
+
 require 'pry'
 
-
-def get_user_input
-  text = gets.chomp 
-  p text 
-end
 
 def prompt_user
   puts "What would you like to do?"
@@ -13,11 +9,19 @@ def prompt_user
   puts "2.) Eat a ham."
 end
 
+def get_user_input
+  num = gets.chomp
+  num
+end
+
 def selection(num)
-  if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+  option_1 = "YUM YUM MUNCH MUNCH MUNCH"
+  option_2 = "HAM HAM HAM IN MY TUMMY"
+  
+  if num == 1
+     p option_1
+  elsif num == 2
+     p option_2
   end
 end
 
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,14 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
-  else
-    string
+  if string[0] == "s" || string[0] == "S"
+     i = 0 
+     ten_s = ""
+    while i < 10 
+     ten_s += string[0] 
+     i += 1
+    end
+   new_string = ten_s + string
   end
+  p new_string
 end
