5f4996ec66784a3150a932b085fc92152cc5d4f5
Samuel Lesser <ineedmoreplates@gmail.com>
2020-04-21 16:29:14 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,19 @@
 # don't forget to add: require 'pry'
+require "pry"
+
 
 def generate_star_date
-  (rand(100000) + 400000) / 10.0
+ star_date = (rand(100000) + 400000) / 10.0
+puts star_date
 end
 
 
-def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+def state_log
+captains_log = "Captain's Log, star date #{generate_star_date}."
+puts captains_log
 end
 
 def engage
-  puts state_log(date)
-  date = generate_star_date
+return state_log
 end
+
